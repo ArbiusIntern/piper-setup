0c8debd9e635ec8cc0819e83ea9646fa63a78ddd
flukexp <flukelolza@gmail.com>
2024-03-24 01:46:19 +0700

    Coqui Local TTS
    implemented invalid voiceid exception handler

--- src/features/coquiLocal/coquiLocal.ts
+++ src/features/coquiLocal/coquiLocal.ts
@@ -3,6 +3,10 @@ import { config } from '@/utils/config';
 export async function coquiLocal(
   message: string,
 ) {
+  const voiceId = config("coquiLocal_voiceid");
+  if (!voiceId) {
+    throw new Error("Invalid CoquiLocal TTS Voice Id");
+  }
   try {
     const res = await fetch(config("coquiLocal_url"), {
       method: 'POST',
